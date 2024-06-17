40e17e148f77761e3b44e2130afbc704b53be4e3
sunhao <sunhao@jixiang.cn>
2024-06-17 18:55:41 +0800

    1.0.1

--- RXInstagramSDK.framework/Headers/RXInstagramService.h
+++ RXInstagramSDK.framework/Headers/RXInstagramService.h
@@ -13,6 +13,14 @@ NS_ASSUME_NONNULL_BEGIN
 @interface RXInstagramService : NSObject
 
 /**
+ * 初始化 instagram
+ * @param clientID 应用ID
+ * @param redirectURI 应用重定向网址
+ * 注册应用时自动生成应用ID，并设置重定向网址redirectURI
+ */
+- (void)initWithClientID:(NSString *)clientID redirectURI:(NSString *)redirectURI;
+
+/**
  * 获取SDK实例（单例）
  */
 + (instancetype)sharedSDK;
